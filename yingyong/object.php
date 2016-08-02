319e666aa94a886a59d887da7535b90015fc26d7
hisheng <465908774@qq.com>
2016-08-02 14:20:08 +0800

    对象和赋值更新
    对象和赋值更新

--- yingyong/object.php
+++ yingyong/object.php
@@ -8,27 +8,79 @@
 
 class A{
     public $testa = 3;
-
 }
 
+class B{
+    public $testa = 7;
+}
 $a = new A();
+$b = $a;        // $a ,$b都是同一个标识符的拷贝
+                // ($a) = ($b) = <id>
 
-$b = $a;
-
+/*
+ * 此时$a,$b的关系：
+ *        +-----------+      +-----------------+
+ * $a --> | object id | ---> | object(Class A) |
+ *        +-----------+      +-----------------+
+ *                               ^
+ *        +-----------+          |
+ * $b --> | object id | ---------+
+ *        +-----------+
+ *
+ *
+ */
 $b->testa = 2;
-
-echo $b->testa;
-
-echo $a->testa;
+echo $b->testa;  //2
+echo $a->testa;  //2
 
 $a->testa = 5;
-echo $b->testa;
-echo $a->testa;
+echo $b->testa;  //5
+echo $a->testa;  //5
 
-$c =  new A();
-echo $c->testa;
+$c =  new B();
 $b =$c;
-echo $b->testa;
-echo $c->testa;
+
+/*
+ * 此时$a,$b,$c的关系：
+ *        +-----------+      +-----------------+
+ * $a --> | object id | ---> | object(Class A) |
+ *        +-----------+      +-----------------+
+ *
+ *        +------------+
+ * $b --> | object id2 | -------------+
+ *        +------------+              |
+ *                                    v
+ *        +------------+      +-----------------+
+ * $c --> | object id2 | ---> | object(Class B) |
+ *        +------------+      +-----------------+
+ */
 
 
+echo $a->testa;  //5
+echo $b->testa;  //7
+echo $c->testa;  //7
+
+//此时如果,把$a重新赋值呢
+
+/*
+ * 此时$a,$b,$c的关系：
+ *        +-----------+
+ * $a --> | object id | ----------------+
+ *        +-----------+                 |
+ *                                     |
+ *        +------------+              v
+ * $b --> | object id2 | -------------+
+ *        +------------+              |
+ *                                    v
+ *        +------------+      +-----------------+
+ * $c --> | object id2 | ---> | object(Class B) |
+ *        +------------+      +-----------------+
+ */
+
+$a = $c;
+echo $a->testa; //7
+
+$a->testa = 8;
+echo $a->testa;  //8
+echo $b->testa;  //8
+echo $c->testa;  //8
