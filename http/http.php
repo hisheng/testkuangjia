f8a421c1c86b60e6d2131437b19599c593a878ed
hisheng <465908774@qq.com>
2016-08-08 17:02:12 +0800

    模拟http
    模拟http

--- http/http.php
+++ http/http.php
@@ -6,18 +6,19 @@
  * Time: 15:07
  */
 
+namespace http;
+
 class http{
+ 
 
-
-    //1 模拟一个 有返回结果数据的  CURLOPT_RETURNTRANSFER = 1
-    public function test(){
+   public function get(){
         // 创建一个cURL资源
         $ch = curl_init();
 
         // 设置URL和相应的选项
         curl_setopt($ch, CURLOPT_URL, "https://mm.taobao.com/tstar/search/tstar_model.do?_input_charset=utf-8");
         curl_setopt($ch, CURLOPT_HEADER, 0);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); //如果把这行注释掉的话，就会直接输出
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); //有返回结果数据的  CURLOPT_RETURNTRANSFER = 1
 
         //当请求https的数据时，会要求证书，这时候，加上下面这两个参数，规避ssl的证书检查
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);    // https请求 不验证证书和hosts
@@ -32,34 +33,18 @@ class http{
         return $result;
     }
 
-    public function get(){
+    public function post($url,$params){
+
+        //参数相关
+        //$url = "https://mm.taobao.com/tstar/search/tstar_model.do?_input_charset=utf-8";
+        $postparams =  http_build_query($params);
+
+
         // 创建一个cURL资源
         $ch = curl_init();
 
         // 设置URL和相应的选项
-        curl_setopt($ch, CURLOPT_URL, "https://mm.taobao.com/tstar/search/tstar_model.do?_input_charset=utf-8");
-        curl_setopt($ch, CURLOPT_HEADER, 0);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); //如果把这行注释掉的话，就会直接输出
-
-        //当请求https的数据时，会要求证书，这时候，加上下面这两个参数，规避ssl的证书检查
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);    // https请求 不验证证书和hosts
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
-
-        // 抓取URL并把它传递给浏览器
-        $result = curl_exec($ch);
-
-        // 关闭cURL资源，并且释放系统资源
-        curl_close($ch);
-
-        return $result;
-    }
-
-    public function post(){
-        // 创建一个cURL资源
-        $ch = curl_init();
-
-        // 设置URL和相应的选项
-        curl_setopt($ch, CURLOPT_URL, "https://mm.taobao.com/tstar/search/tstar_model.do?_input_charset=utf-8");
+        curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_HEADER, 0);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); //如果把这行注释掉的话，就会直接输出
 
@@ -69,9 +54,8 @@ class http{
 
 
         //post方式
-        $params =  "currentPage=2";
         curl_setopt($ch, CURLOPT_POST, 1);    // post 提交方式
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $postparams);
 
 
 
@@ -89,5 +73,3 @@ class http{
 }
 
 
-$s  = new http();
-print_r($s->post());
---
+++ http/test.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhanghaisheng
+ * Date: 2016/8/8
+ * Time: 16:58
+ */
+
+namespace test;
+
+require 'http.php';
+
+
+// 1
+$t = new \http\http();
+print_r($t->test());
+
+//2
+
+$s  = new \http\http();
+$url = "https://mm.taobao.com/tstar/search/tstar_model.do?_input_charset=utf-8";
+$params = array('currentPage'=>3,'pageSize'=>200);
+
+print_r($s->post($url,$params));
