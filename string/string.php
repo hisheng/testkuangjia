00a8b1250e9c83ce208ef18bf47b7d9771dd154f
hisheng <465908774@qq.com>
2016-08-09 17:43:50 +0800

    完善字符串
    完善字符串

--- string/string.php
+++ string/string.php
@@ -69,6 +69,9 @@ class string{
         echo stripos($s,'Y').br;//21
         // 因为有可能是 0 所以用if 判断查找失败 用 === false 比较适合
 
+        //3.1  查找字符，在字符串最后一次次出现的位置 ,返回后面的字符串
+        echo strrchr($s,'o').br;
+
         //4 找字符，在字符串首次出现的位置 ,返回 自己包括后面的字符串
         // 区分大小写
         echo strstr($s,'Y').br; //You , ha ha
@@ -78,11 +81,21 @@ class string{
         //5 查找字符，在字符串首次出现的位置 ,返回前面还有多少个字符串
         echo strcspn($s,'l').br; // 2
         //签名有多少个字符，竟然和这个字符所处的index是相等的，哦哦
-        echo strcspn($s,'l').' = '.stripos($s,'l');//2 = 2
+        echo strcspn($s,'l').' = '.stripos($s,'l').br;//2 = 2
 
 
-        //6
+        //6重复一个字符
+        echo str_repeat("-=", 10).br; //-=-=-=-=-=-=-=-=-=-= 重复了10遍
 
+
+        //7 输出子字符串，切割字符串， substr($string,$start,$length);
+        echo substr("abcdef", 0, 6).br; // abcdef
+        echo substr("abcdef", 0, 1).br;// a
+        echo substr("abcdef", 0, -1).br; //abcde
+
+
+        //8 替换 substr_replace($string,$replace,$start,$length);
+        echo substr_replace($s,'123456',0,6);//123456World , i love You , ha ha
 
 
 
