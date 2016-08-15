00863f59d78ea3d826eb7045e4ef9a9a3222d49f
hisheng <465908774@qq.com>
2016-08-15 15:06:09 +0800

    PHP 时间 DateTime 类 学习
    PHP 时间 DateTime 类 学习

---
+++ datetime/index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhanghaisheng
+ * Date: 2016/8/15
+ * Time: 14:59
+ */
+
+$dt = new DateTime('17:00');
+echo $dt->format('Y 年 m 月 d 日 H:i:s');
+
+echo $dt->format('U');
+
