92bcc7600a2d2e8603a2d54acd465a9560ea0c41
hisheng <465908774@qq.com>
2016-08-25 10:56:34 +0800

    测试 自定义全局 namespace 自动加载
    composer install 命令

---
+++ Composertest/Test.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhanghaisheng
+ * Date: 2016/8/25
+ * Time: 10:09
+ */
+
+require '../vendor/autoload.php';
+
+
+\TestKuangjia\Hisheng::test();
---
+++ src/Hisheng.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhanghaisheng
+ * Date: 2016/8/25
+ * Time: 10:06
+ */
+namespace TestKuangjia;
+
+class Hisheng{
+    public static function test(){
+        echo 'hisheng testkuangjia test';
+    }
+}
