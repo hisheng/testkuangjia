6c563ae9075dfbc064c937dbdead7d6a1936f0b2
hisheng <465908774@qq.com>
2016-09-05 15:39:42 +0800

    反转字符串

---
+++ cookbook/strrev.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhanghaisheng
+ * Date: 2016/9/5
+ * Time: 15:28
+ */
+function br(){
+    echo '<br/>';
+}
+//反转字符串
+
+//1. 按字节反转
+
+$a = 'abc def ghi jkl mno pqrst';
+
+echo strrev($a);
+
+br();
+
+// 按单词 反转
+var_dump(implode(' ',array_reverse(explode(' ',$a))));
