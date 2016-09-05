4944642b267486256e87a37379c8db414ea2959b
hisheng <465908774@qq.com>
2016-09-05 11:12:00 +0800

    增加 string字符串 单字节，以及字符串单引号，双引号的理解

---
+++ cookbook/string.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhanghaisheng
+ * Date: 2016/9/5
+ * Time: 9:58
+ */
+
+//1 utf8
+
+
+$a = 'abc123 h$';
+$b = '海生';
+var_dump($a); //string 'abc123 h$' (length=9)
+var_dump(utf8_encode($a));//string 'abc123 h$' (length=9)
+
+//中文的问题
+var_dump($b);//string '海生' (length=6)
+var_dump(utf8_encode($b));//string 'æµ·ç' (length=12)
+var_dump(utf8_decode(utf8_encode($b)));//string '海生' (length=6)
+
+
+//二进制 安全
+var_dump('01010111');//string '01010111' (length=8)
+
+//单引号，需要转义 单引号
+var_dump('I have gone to the store'); //string 'I have gone to the store' (length=24)
+var_dump('i\'ve gone to the store');//string 'i've gone to the store' (length=22)
+var_dump('would you pay $1.75 for 8 ounces of tap water?');//string 'would you pay $1.75 for 8 ounces of tap water?' (length=46)
+var_dump('in double-quoted string , newline is represented by \n');//string 'in double-quoted string , newline is represented by \n' (length=54)
+
+//双引号，需要转义 双引号 , $符合
+
+var_dump("i've gone to the store"); //string 'i've gone to the store' (length=22)
+var_dump("the sauce cost \$10.25"); //string 'the sauce cost $10.25' (length=21)
+$cost = '$10.25';
+var_dump("the cost is $cost"); //string 'the cost is $10.25' (length=18)
+
+
+//获取单个字节
+$neighbor = 'hilde';
+var_dump($neighbor[3]); //string 'd' (length=1)
+
