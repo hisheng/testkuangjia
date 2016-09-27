3a6e2d383a2c4151a3fac27f748004bcc680afd3
hisheng <465908774@qq.com>
2016-09-27 18:48:34 +0800

    支持的一些 网络协议

--- manual/protocol.php
+++ manual/protocol.php
@@ -8,9 +8,17 @@
 
 // http:// 协议
 $url = 'http://localhost/testkuangjia/manual/yield.php';
-
 $fp = fopen($url,'r');
-
 $meta_data = stream_get_meta_data($fp);
-
 var_dump($meta_data);
+
+
+
+
+
+$file = 'file://D:/sign.zip';
+$fp2 = fopen($file,'r');
+$meta_data2 = stream_get_meta_data($fp2);
+var_dump($meta_data2);
+
+
---
+++ manual/stream/s1.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhanghaisheng
+ * Date: 2016/9/27
+ * Time: 18:26
+ */
+
