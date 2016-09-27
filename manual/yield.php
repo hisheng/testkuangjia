aeb407d4fc31705a969a3f287c32b170148aee67
hisheng <465908774@qq.com>
2016-09-27 18:03:39 +0800

    生成器 生成多个返回值

---
+++ manual/yield.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhanghaisheng
+ * Date: 2016/9/27
+ * Time: 17:38
+ */
+//一个生成器函数看起来像一个普通的函数，不同的是普通函数返回一个值，而一个生成器可以yield生成许多它所需要的值。
+
+function xrange($start, $limit, $step = 1){
+    if ($start < $limit) {
+        if ($step <= 0) {
+            throw new LogicException('Step must be +ve');
+        }
+
+        for ($i = $start; $i <= $limit; $i += $step) {
+            yield $i;
+        }
+    } else {
+        if ($step >= 0) {
+            throw new LogicException('Step must be -ve');
+        }
+
+        for ($i = $start; $i >= $limit; $i += $step) {
+            yield $i;
+        }
+    }
+}
+
+foreach (xrange(1, 9, 2) as $number) {
+    echo "$number ";
+}
+
+var_dump(xrange(1, 9, 2));
+
+
+function gen_one_to_three() {
+    for ($i = 1; $i <= 3; $i++) {
+        //注意变量$i的值在不同的yield之间是保持传递的。
+        yield $i;
+    }
+}
+
+$generator = gen_one_to_three();
+var_dump($generator);
+
+foreach ($generator as $value) {
+    echo "{$value}<br/>";
+}
